aaf769738daae262bc0aabb4fa4c15bbb2771741
Ahmet Kaya <shortsforturkey@gmail.com>
2025-07-14 12:05:11 +0300

    refactor: update YTFetcher class

--- ytfetcher/core.py
+++ ytfetcher/core.py
@@ -1,22 +1,21 @@
-import httpx
-from youtube_transcript_api import YouTubeTranscriptApi
 from ytfetcher.youtube_v3 import YoutubeV3
-from scripts.headers import get_realistic_headers
+from ytfetcher.types.channel import ChannelData
+from ytfetcher.transcript_fetcher import TranscriptFetcher
+import httpx
+
+timeout = httpx.Timeout(connect=2.0, read=2.0, write=2.0, pool=2.0)
 
 class YTFetcher:
-    def __init__(self, yt_client: YoutubeV3, channel_name: str, proxy=None, max_results: int = 50, timeout: httpx.Timeout | None = None, headers: dict | None = None):
+    def __init__(self, yt_client: YoutubeV3):
         self.yt_client = yt_client
-        self.channel_name = channel_name
-        self.proxy = proxy
-        self.max_results = max_results
+        self.channel_data = self.yt_client.fetch_channel_snippets()
+        self.fetcher = TranscriptFetcher(self.channel_data.video_ids, self.channel_data.metadata, timeout=timeout)
 
-        self.headers = headers or get_realistic_headers()
-        self.timeout = timeout or httpx.Timeout(10.0)
-    
-        self.yt_api = YouTubeTranscriptApi(http_client=httpx.Client(timeout=self.timeout, headers=self.headers))
-    
     def fetch_transcripts(self):
         pass
 
-    def fetch_transcripts_with_snippets(self):
-        pass
+    async def fetch_transcripts_with_metadata(self):
+        return await self.fetcher.fetch()
+    
+    def get_channel_data(self) -> ChannelData:
+        return self.channel_data
