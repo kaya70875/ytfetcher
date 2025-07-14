f706345ee8e0ce1597d8afab7a0c572f80afcf22
Ahmet Kaya <shortsforturkey@gmail.com>
2025-07-14 12:05:25 +0300

    refactor: Update YoutubeV3 class

--- ytfetcher/youtube_v3.py
+++ ytfetcher/youtube_v3.py
@@ -2,27 +2,20 @@ import httpx
 from ytfetcher.types.channel import ChannelData
 
 class YoutubeV3:
-    def __init__(self, api_key: str, snippets: dict):
+    def __init__(self, api_key: str, channel_name: str, max_results: int = 50):
         self.api_key = api_key
-        self.snippets = snippets
+        self.channel_name = channel_name
+        self.max_results = max_results
     
-    def fetch_channel_snippets(self, channel_id: str):
+    def fetch_channel_snippets(self):
         """
         Get all channel snippets from a YouTube channel using the YouTube Data API v3.
         """
 
-        url = 'https://www.googleapis.com/youtube/v3/channels'
-        params = {
-            'part': 'contentDetails',
-            'id': channel_id,
-            'key': self.api_key
-        }
+        channel_id = self._get_channel_id()
 
-        with httpx.Client(timeout=self.timeout) as client:
-            response = client.get(url, params=params)
-            res = response.json()
-            uploads_playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-            return self._fetch_with_playlist_id(uploads_playlist_id, self.max_results)
+        uploads_playlist_id = self._get_upload_playlist_id(channel_id)
+        return self._fetch_with_playlist_id(uploads_playlist_id)
     
     def _fetch_with_playlist_id(self, uploads_playlist_id: str) -> ChannelData:
         try:
@@ -33,12 +26,12 @@ class YoutubeV3:
             base_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
             next_page_token = None
 
-            with httpx.Client(timeout=self.timeout) as client:
+            with httpx.Client() as client:
                 while True:
                     params = {
                         'part': 'snippet',
                         'playlistId': uploads_playlist_id,
-                        'maxResults': 50,
+                        'maxResults': self.max_results,
                         'pageToken': next_page_token,
                         'key': self.api_key,
                         'fields': 'items(snippet(title,description,publishedAt,channelId,thumbnails,resourceId/videoId)),nextPageToken'
@@ -62,3 +55,35 @@ class YoutubeV3:
         except AttributeError as attr_err:
             print('Error fetching video IDs:', attr_err)
             return ChannelData(video_ids=[], metadata=[])
+    
+    def _get_upload_playlist_id(self, channel_id: str):
+        url = 'https://www.googleapis.com/youtube/v3/channels'
+        params = {
+            'part': 'contentDetails',
+            'id': channel_id,
+            'key': self.api_key
+        }
+
+        with httpx.Client() as client:
+            response = client.get(url, params=params)
+            res = response.json()
+            uploads_playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+            return uploads_playlist_id
+    
+    def _get_channel_id(self) -> str:
+        with httpx.Client() as client:
+            response = client.get(
+                'https://www.googleapis.com/youtube/v3/channels',
+                params={
+                    'part': 'id',
+                    'forHandle': f'@{self.channel_name}',
+                    'key': self.api_key
+                }
+            )
+            response.raise_for_status()
+            res = response.json()
+
+            if 'items' in res and res['items']:
+                return res['items'][0]['id']
+            else:
+                raise ValueError(f"Channel '{self.channel_name}' not found.")
