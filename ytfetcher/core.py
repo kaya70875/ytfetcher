c4f010554aaf02a18be7da1cca4630f5e21fc98f
Ahmet Kaya <shortsforturkey@gmail.com>
2025-07-16 13:52:24 +0300

    feat: implement proxy config

--- ytfetcher/core.py
+++ ytfetcher/core.py
@@ -2,32 +2,35 @@ from ytfetcher.youtube_v3 import YoutubeV3
 from ytfetcher.types.channel import ChannelData, FetchAndMetaResponse, Transcript
 from ytfetcher.transcript_fetcher import TranscriptFetcher
 from ytfetcher.config.http_config import HTTPConfig
+from youtube_transcript_api.proxies import ProxyConfig
 
 class YTFetcher:
-    def __init__(self, api_key: str, http_config: HTTPConfig, max_results: int, video_ids: list[str], channel_handle: str | None = None):
+    def __init__(self, api_key: str, http_config: HTTPConfig, max_results: int, video_ids: list[str], channel_handle: str | None = None, proxy_config: ProxyConfig | None = None):
         self.v3 = YoutubeV3(api_key=api_key, channel_name=channel_handle, video_ids=video_ids, max_results=max_results)
         self.snippets = self.v3.fetch_channel_snippets()
         self.http_config = http_config
+        self.proxy_config = proxy_config
+
         self._fetcher: TranscriptFetcher | None = None
 
     def _get_fetcher(self) -> TranscriptFetcher:
         if self._fetcher is None:
-            self._fetcher = TranscriptFetcher(self.snippets.video_ids, self.snippets.metadata, http_config=self.http_config)
+            self._fetcher = TranscriptFetcher(self.snippets.video_ids, self.snippets.metadata, http_config=self.http_config, proxy_config=self.proxy_config)
         return self._fetcher
     
     @classmethod
-    def from_channel(cls, api_key: str, channel_handle: str, max_results: int = 50, http_config: HTTPConfig = HTTPConfig()) -> "YTFetcher":
+    def from_channel(cls, api_key: str, channel_handle: str, max_results: int = 50, http_config: HTTPConfig = HTTPConfig(), proxy_config: ProxyConfig | None = None) -> "YTFetcher":
         """
         Create a fetcher that pulls up to max_results from the channel.
         """
-        return cls(api_key=api_key, http_config=http_config, max_results=max_results, video_ids=[], channel_handle=channel_handle)
+        return cls(api_key=api_key, http_config=http_config, max_results=max_results, video_ids=[], channel_handle=channel_handle, proxy_config=proxy_config)
     
     @classmethod
-    def from_video_ids(cls, api_key: str, video_ids: list[str] = [], http_config: HTTPConfig = HTTPConfig()) -> "YTFetcher":
+    def from_video_ids(cls, api_key: str, video_ids: list[str] = [], http_config: HTTPConfig = HTTPConfig(), proxy_config: ProxyConfig | None = None) -> "YTFetcher":
         """
         Create a fetcher that only fetches from given video ids.
         """
-        return cls(api_key=api_key, http_config=http_config, max_results=len(video_ids), video_ids=video_ids, channel_name=None)
+        return cls(api_key=api_key, http_config=http_config, max_results=len(video_ids), video_ids=video_ids, channel_name=None, proxy_config=proxy_config)
 
     async def get_transcripts(self) -> Transcript:
         return [x.transcript for x in await self._get_fetcher().fetch()]
--- ytfetcher/transcript_fetcher.py
+++ ytfetcher/transcript_fetcher.py
@@ -1,9 +1,10 @@
-from ytfetcher.types.channel import Snippet
 from youtube_transcript_api._errors import NoTranscriptFound, VideoUnavailable, TranscriptsDisabled
 from youtube_transcript_api import YouTubeTranscriptApi
-from concurrent.futures import ThreadPoolExecutor
+from youtube_transcript_api.proxies import ProxyConfig
+from ytfetcher.types.channel import Snippet
 from ytfetcher.types.channel import FetchAndMetaResponse
 from ytfetcher.config.http_config import HTTPConfig
+from concurrent.futures import ThreadPoolExecutor
 import asyncio
 import httpx
 import logging
@@ -11,10 +12,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 class TranscriptFetcher:
-    def __init__(self, video_ids: list[str], snippets: list[Snippet], http_config: HTTPConfig):
+    def __init__(self, video_ids: list[str], snippets: list[Snippet], http_config: HTTPConfig, proxy_config: ProxyConfig | None = None):
         self.video_ids = video_ids
         self.snippets = snippets
         self.executor = ThreadPoolExecutor(max_workers=30)
+        self.proxy_config = proxy_config
     
         self.httpx_client = httpx.Client(timeout=http_config.timeout, headers=http_config.headers)
 
@@ -36,7 +38,7 @@ class TranscriptFetcher:
 
     def _fetch_single(self, video_id: str, snippet: Snippet) -> dict | None:
         try:
-            yt_api = YouTubeTranscriptApi(http_client=self.httpx_client)
+            yt_api = YouTubeTranscriptApi(http_client=self.httpx_client, proxy_config=self.proxy_config)
             transcript = yt_api.fetch(video_id).to_raw_data()
             logger.info(f'{video_id} fetched.')
             return {
