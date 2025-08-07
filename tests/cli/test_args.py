83e00cc4def6d14394730c50e8a9d27d5a1d9bec
Ahmet Kaya <shortsforturkey@gmail.com>
2025-08-07 19:16:09 +0300

    test: add tests for cli arguments

---
+++ tests/cli/test_args.py
@@ -0,0 +1,73 @@
+from ytfetcher._cli import create_parser, YTFetcherCLI
+from ytfetcher._core import YTFetcher
+
+def test_initialize_http_and_proxy_config_default():
+    parser = create_parser()
+    args = parser.parse_args([
+        "from_channel",
+        "fake_api_key",
+        "-c", "TheOffice"
+    ])
+
+    cli = YTFetcherCLI(args=args)
+
+    assert cli.args.http_timeout == 4.0
+    assert cli.args.http_headers == cli.http_config.headers
+    
+    assert cli.args.http_proxy == ''
+    assert cli.args.https_proxy == ''
+
+    assert cli.args.webshare_proxy_username is None
+    assert cli.args.webshare_proxy_password is None
+
+def test_initialize_http_and_proxy_config_override():
+    parser = create_parser()
+    args = parser.parse_args([
+        "from_channel",
+        "fake_api_key",
+        "-c", "TheOffice",
+        "--http-timeout", "4.2",
+        "--http-headers", "{'key': 'value'}",
+        "--http-proxy", "testhttp:proxy",
+        "--https-proxy", "testhttps:proxy"
+    ])
+
+    cli = YTFetcherCLI(args=args)
+
+    assert cli.args.http_timeout == 4.2
+    assert cli.args.http_headers == {'key': 'value'}
+
+    assert cli.args.http_proxy == 'testhttp:proxy'
+    assert cli.args.https_proxy == 'testhttps:proxy'
+
+def test_webshare_proxy_config():
+    parser = create_parser()
+    args = parser.parse_args([
+        "from_channel",
+        "fake_api_key",
+        "-c", "TheOffice",
+        "--http-timeout", "4.2",
+        "--http-headers", "{'key': 'value'}",
+        "--webshare-proxy-username", "username",
+        "--webshare-proxy-password", "password"
+    ])
+
+    cli = YTFetcherCLI(args=args)
+
+    assert cli.args.webshare_proxy_username == 'username'
+    assert cli.args.webshare_proxy_password == 'password'
+
+def test_export_arguments():
+    parser = create_parser()
+    args = parser.parse_args([
+        "from_channel",
+        "fake_api_key",
+        "-c", "TheOffice",
+        "-f", "json",
+        "-o", "C:/Users/user1/Desktop"
+    ])
+
+    cli = YTFetcherCLI(args=args)
+
+    assert cli.args.format == 'json'
+    assert cli.args.output_dir == 'C:/Users/user1/Desktop'
