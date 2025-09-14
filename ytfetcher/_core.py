db2a549103382a71f1c642c1486b897f8bb5f919
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-14 14:16:28 +0300

    update ytfetcher return models

--- ytfetcher/_core.py
+++ ytfetcher/_core.py
@@ -1,6 +1,6 @@
 from ytfetcher._youtube_v3 import YoutubeV3
 from ytfetcher._youtube_dl import YoutubeDL
-from ytfetcher.models.channel import ChannelData, Snippet, VideoTranscript, VideoMetadata
+from ytfetcher.models.channel import ChannelData, Snippet
 from ytfetcher._transcript_fetcher import TranscriptFetcher
 from ytfetcher.config.http_config import HTTPConfig
 from youtube_transcript_api.proxies import ProxyConfig
@@ -34,7 +34,7 @@ class YTFetcher:
         self.youtube_dl = None
 
         self.v3 = None
-        self.snippets: list[Snippet] = []
+        self.snippets = []
 
         if api_key:
             self.v3 = YoutubeV3(api_key=api_key, channel_name=channel_handle, video_ids=video_ids, max_results=max_results, http_config=self.http_config)
@@ -74,7 +74,7 @@ class YTFetcher:
             return transcripts
         
         for transcript, snippet in zip(transcripts, self.snippets):
-            transcript.metadata = snippet
+            transcript.metadata = snippet.metadata
         
         return transcripts
     
@@ -88,7 +88,7 @@ class YTFetcher:
         
         return await self.fetcher.fetch()
 
-    async def fetch_snippets(self) -> list[ChannelData]:
+    async def fetch_snippets(self) -> list[ChannelData] | None:
         """
         Returns the raw snippet data (metadata and video IDs) retrieved from the YouTube Data API.
 
@@ -96,15 +96,7 @@ class YTFetcher:
             list[ChannelData] | None: An object containing video metadata and IDs.
         """
 
-        transcripts = await self.fetcher.fetch()
-
-        return [
-            ChannelData(
-                video_id=transcript.video_id,
-                metadata=snippet
-            )
-            for transcript, snippet in zip(transcripts, self.snippets)
-        ]
+        return self.snippets
 
     @property
     def video_ids(self) -> list[str]:
@@ -127,7 +119,7 @@ class YTFetcher:
         Returns:
             list[Snippet] | None: List of Snippet objects containing video metadata.
         """
-        return self.snippets
+        return [snippet.metadata for snippet in self.snippets]
     
     def _get_video_ids(self) -> list[str]:
         """
