30a428b14418e8cd7dfc4c9b235f4c4983a4d450
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-27 12:38:16 +0300

    refactor: remove YoutubeV3 base class exceptions

--- ytfetcher/exceptions.py
+++ ytfetcher/exceptions.py
@@ -1,8 +1,3 @@
-class YoutubeV3Error(Exception):
-    """
-    Base exception for all Youtube V3 api errors.
-    """
-
 class YTFetcherError(Exception):
     """
     Base exception for all YTFetcher errors.
@@ -32,24 +27,3 @@ class InvalidHeaders(YTFetcherError):
     """
     Raises when headers are invalid.
     """
-
-class InvalidChannel(YoutubeV3Error):
-    """
-    Raises when channel handle is invalid or cannot found.
-    """
-
-class InvalidApiKey(YoutubeV3Error):
-    """
-    Raises when api key for Youtube V3 is invalid.
-    """
-
-class MaxResultsExceed(YoutubeV3Error):
-    """
-    Raises when max_results bigger than 500 videos.
-    """
-
-class NoChannelVideosFound(YoutubeV3Error):
-    """
-    Raises when a channel has no videos to fetch.
-    """
-
