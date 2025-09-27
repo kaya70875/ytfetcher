a634dcac3b2b0f06ee38e835860d728bcb81eed0
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-27 12:32:45 +0300

    refactor: update fetch method to return DLSnippet objects instead of video IDs

--- ytfetcher/_youtube_dl.py
+++ ytfetcher/_youtube_dl.py
@@ -1,4 +1,6 @@
 import yt_dlp
+from yt_dlp.utils import DownloadError
+from ytfetcher.models.channel import DLSnippet
 
 class YoutubeDL:
     """
@@ -10,18 +12,36 @@ class YoutubeDL:
     def __init__(self, channel_handle: str, max_results: int = 50):
         self.channel_handle = channel_handle
         self.max_results = max_results
-        self.video_ids = self._get_video_ids()
 
-    def _get_video_ids(self) -> list[str]:
-        ydl_opts = {
-            'quiet': True,
-            'extract_flat': 'in_playlist',
-            'skip_download': True,
-            'playlistend': self.max_results
-        }
+    def fetch(self) -> list[DLSnippet]:
+        try:
+            ydl_opts = {
+                'quiet': True,
+                'extract_flat': 'in_playlist',
+                'skip_download': True,
+                'playlistend': self.max_results
+            }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            full_url = f"https://www.youtube.com/@{self.channel_handle}/videos"
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                full_url = f"https://www.youtube.com/@{self.channel_handle}/videos"
 
-            info = ydl.extract_info(full_url, download=False)
-            return [e['id'] for e in info['entries'] if e.get('id')]
+                info = ydl.extract_info(full_url, download=False)
+                entries = [e for e in info['entries'] if e]
+
+                return [
+                    DLSnippet(
+                        video_id=entry['id'],
+                        title=entry['title'],
+                        description=entry['description'],
+                        url=entry['url'],
+                        duration=entry['duration'],
+                        view_count=entry['view_count'],
+                        thumbnails=entry['thumbnails']
+                    )
+                    for entry in entries
+                ]
+        except DownloadError as download_err:
+            raise download_err
+
+        except Exception as exc:
+            raise exc
