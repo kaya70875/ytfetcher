abe03c4b6bed41f4e13fdde0e815b04e7544994e
Ahmet Kaya <shortsforturkey@gmail.com>
2025-10-16 19:55:30 +0300

    tests: add tests for CLI usage of PlaylistFetcher

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -3,6 +3,8 @@ from unittest.mock import patch, AsyncMock
 from ytfetcher._cli import YTFetcherCLI, create_parser
 from ytfetcher.config import HTTPConfig
 
+# --> Basic Call Tests <--
+
 @pytest.mark.asyncio
 @patch('ytfetcher._cli.Exporter.export_as_txt')
 @patch.object(YTFetcherCLI, '_run_fetcher')
@@ -30,6 +32,22 @@ async def test_run_from_video_ids_called(mock_run_from_video_ids, mock_export_as
     await cli.run()
 
     mock_run_from_video_ids.assert_called_once()
+
+@pytest.mark.asyncio
+@patch('ytfetcher._cli.Exporter.export_as_txt')
+@patch.object(YTFetcherCLI, '_run_fetcher')
+async def test_run_from_playlist_id_called(mock_run_from_playlist_id, mock_export_as_txt):
+    parser = create_parser()
+    args = parser.parse_args([
+        "from_playlist_id",
+    ])
+
+    cli = YTFetcherCLI(args=args)
+    await cli.run()
+
+    mock_run_from_playlist_id.assert_called_once()
+
+# --> Arguments Test <--
 
 @pytest.mark.asyncio
 @patch('ytfetcher._cli.YTFetcherCLI._initialize_http_config')
@@ -65,6 +83,36 @@ async def test_run_from_channel_arguments_passed_correctly_to_ytfetcher(mock_ytf
 @patch('ytfetcher._cli.YTFetcherCLI._initialize_http_config')
 @patch('ytfetcher._cli.Exporter.export_as_txt')
 @patch('ytfetcher._cli.YTFetcher')
+async def test_run_from_playlist_id_arguments_passed_correctly_to_ytfetcher(mock_ytfetcher, mock_export_as_txt, mock_initialize_http_config):
+    mock_fetcher = AsyncMock()
+    mock_ytfetcher.from_playlist_id.return_value = mock_fetcher
+
+    # Mock HTTP Config
+    mock_initialize_http_config.return_value = HTTPConfig()
+
+    parser = create_parser()
+    args = parser.parse_args([
+        "from_playlist_id",
+        "-p", "playlistid",
+        "--languages", "en", "de"
+    ])
+
+    cli = YTFetcherCLI(args=args)
+    await cli.run()
+
+    mock_ytfetcher.from_playlist_id.assert_called_once_with(
+        playlist_id="playlistid",
+        http_config=cli._initialize_http_config(),
+        proxy_config=cli._initialize_proxy_config(),
+        languages=["en", "de"]
+    )
+
+    mock_fetcher.fetch_youtube_data.assert_called_once()
+
+@pytest.mark.asyncio
+@patch('ytfetcher._cli.YTFetcherCLI._initialize_http_config')
+@patch('ytfetcher._cli.Exporter.export_as_txt')
+@patch('ytfetcher._cli.YTFetcher')
 async def test_run_from_video_ids_arguments_passed_correctly_to_ytfetcher(mock_ytfetcher, mock_export_as_txt, mock_initialize_http_config):
     mock_fetcher = AsyncMock()
     mock_ytfetcher.from_video_ids.return_value = mock_fetcher
@@ -90,6 +138,8 @@ async def test_run_from_video_ids_arguments_passed_correctly_to_ytfetcher(mock_y
     )
 
     mock_fetcher.fetch_youtube_data.assert_called_once()
+
+# --> Exporter Tests <--
 
 @pytest.mark.asyncio
 @patch('ytfetcher._cli.Exporter.export_as_txt')
@@ -136,7 +186,34 @@ async def test_export_method_from_channel(mock_ytfetcher, mock_exporter_class, m
         "from_channel",
         "-c", "TheOffice"
     ])
-    
+
+    cli = YTFetcherCLI(args=args)
+    await cli.run()
+
+    mock_exporter_class.assert_called_once_with(
+        channel_data='channeldata',
+        output_dir=args.output_dir,
+        filename='data'
+    )
+
+    mock_exporter_instance.export_as_txt.assert_called_once()
+
+@pytest.mark.asyncio
+@patch('ytfetcher._cli.Exporter.export_as_txt')
+@patch('ytfetcher._cli.Exporter')
+@patch('ytfetcher._cli.YTFetcher')
+async def test_export_method_from_playlist_id(mock_ytfetcher, mock_exporter_class, mock_export_as_txt):
+    mock_fetcher = AsyncMock()
+    mock_ytfetcher.from_playlist_id.return_value = mock_fetcher
+    mock_fetcher.fetch_youtube_data.return_value = 'channeldata'
+
+    mock_exporter_instance = mock_exporter_class.return_value
+
+    parser = create_parser()
+    args = parser.parse_args([
+        "from_playlist_id",
+        "-p", "playlistid"
+    ])
 
     cli = YTFetcherCLI(args=args)
     await cli.run()
