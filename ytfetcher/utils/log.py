a0acf663a2b36235e2026d065b7c0aa28a214517
Ahmet Kaya <shortsforturkey@gmail.com>
2025-11-09 11:32:24 +0300

    add docstrings for log.py

--- ytfetcher/utils/log.py
+++ ytfetcher/utils/log.py
@@ -4,6 +4,11 @@ import colorama
 LEVEL = Literal['INFO', 'WARNING', 'DONE', 'ERROR']
 
 def decide_color(level: LEVEL):
+    """
+    Chooses color based on message level.
+    Args:
+        level(LEVEL): Message level.
+    """
     match level:
         case 'INFO':
             return colorama.Fore.CYAN
@@ -15,5 +20,11 @@ def decide_color(level: LEVEL):
             return colorama.Fore.GREEN
 
 def log(message: str, level: LEVEL = ('INFO')):
+    """
+    Print message with different levels and colors. ex: (INFO, WARNING, DONE, ERROR).
+    Args:
+        message(str): The message to be printed.
+        level(LEVEL): Priority level. Default to INFO
+    """
     decided_color = decide_color(level=level)
     return print(decided_color + f'[{level}] ' + colorama.Style.RESET_ALL + message)
