07b96d1207e5b72c4e50a7fa04c1ed9f96d96559
Ahmet Kaya <shortsforturkey@gmail.com>
2025-12-24 18:45:29 +0300

    fix: ensure comments are always returned as a list in CommentFetcher

--- ytfetcher/_youtube_dl.py
+++ ytfetcher/_youtube_dl.py
@@ -51,7 +51,7 @@ class CommentFetcher:
                 
         with yt_dlp.YoutubeDL(ydl_opts_deep) as ydl:
             info_dict = ydl.extract_info(video_url, download=False)
-            data = info_dict.get('comments', None)
+            data = info_dict.get('comments', None) or []
             return [
                 Comment(
                 id=comment.get('id'),
