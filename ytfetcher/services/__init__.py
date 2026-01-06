f40a55feb2dd89098183a6b50af212db95a03081
Ahmet Kaya <shortsforturkey@gmail.com>
2026-01-06 21:57:35 +0300

    feat: add PreviewRenderer to public API in __init__.py

--- ytfetcher/services/__init__.py
+++ ytfetcher/services/__init__.py
@@ -1,8 +1,10 @@
 from .exports import CSVExporter, TXTExporter, JSONExporter, Exporter
+from ._preview import PreviewRenderer
 
 __all__ = [
     'CSVExporter',
     'JSONExporter',
     'TXTExporter',
-    'Exporter'
+    'Exporter',
+    'PreviewRenderer'
 ]
