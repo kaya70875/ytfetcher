2295d651241026e593c1f1c275ea1c99d00e0c2c
Ahmet Kaya <shortsforturkey@gmail.com>
2026-01-15 20:51:47 +0300

    tests: remove async tests

--- tests/cli/test_cli_filters.py
+++ tests/cli/test_cli_filters.py
@@ -1,13 +1,11 @@
-import pytest
-from unittest.mock import patch, AsyncMock
+from unittest.mock import patch, Mock
 from ytfetcher.config import HTTPConfig
 from ytfetcher._cli import create_parser, YTFetcherCLI
 
-@pytest.mark.asyncio
 @patch('ytfetcher._cli.YTFetcherCLI._initialize_http_config')
 @patch('ytfetcher._cli.YTFetcher')
-async def test_run_filter_argument_passed_correctly_to_ytfetcher(mock_ytfetcher, mock_initialize_http_config):
-    mock_fetcher = AsyncMock()
+def test_run_filter_argument_passed_correctly_to_ytfetcher(mock_ytfetcher, mock_initialize_http_config):
+    mock_fetcher = Mock()
     mock_ytfetcher.from_channel.return_value = mock_fetcher
 
     mock_initialize_http_config.return_value = HTTPConfig()
@@ -21,7 +19,7 @@ async def test_run_filter_argument_passed_correctly_to_ytfetcher(mock_ytfetcher,
     ])
 
     cli = YTFetcherCLI(args=args)
-    await cli.run()
+    cli.run()
 
     args, kwargs = mock_ytfetcher.from_channel.call_args
     passed_filters = kwargs.get('filters')
@@ -31,7 +29,7 @@ async def test_run_filter_argument_passed_correctly_to_ytfetcher(mock_ytfetcher,
     assert callable(passed_filters[0])
     assert callable(passed_filters[1])
 
-    mock_fetcher.fetch_youtube_data.assert_awaited_once()
+    mock_fetcher.fetch_youtube_data.assert_called_once()
 
 def test_get_active_filters():
     parser = create_parser()
