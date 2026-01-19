48a7b04aa0ac8dbcd4e8e164116e63da89bbd980
Ahmet Kaya <shortsforturkey@gmail.com>
2026-01-19 07:46:52 +0300

    fix: typo and add max_results

--- ytfetcher/_cli.py
+++ ytfetcher/_cli.py
@@ -173,6 +173,7 @@ class YTFetcherCLI:
                 self._run_fetcher(
                     YTFetcher.from_playlist_id,
                     playlist_id=self.args.playlist_id,
+                    max_results=self.args.max_results,
                     languages=self.args.languages,
                     manually_created=self.args.manually_created
                 )
@@ -182,6 +183,7 @@ class YTFetcherCLI:
                 self._run_fetcher(
                     YTFetcher.from_search,
                     query=self.args.search,
+                    max_results=self.args.max_results,
                     languages=self.args.languages,
                     manually_created=self.args.manually_created
                 )
@@ -213,7 +215,7 @@ def create_parser() -> argparse.ArgumentParser:
     # From search parsers
     parser_search = subparsers.add_parser("search", help="Fetch data from youtube with search query.")
     parser_search.add_argument("search", type=str, help="The query to search from Youtube.")
-    parser_search.add_argument("-m", "--max-resuts", type=int, default=20, help="Maximum videos to fetch.")
+    parser_search.add_argument("-m", "--max-results", type=int, default=20, help="Maximum videos to fetch.")
     _create_common_arguments(parser_search)
 
     return parser
